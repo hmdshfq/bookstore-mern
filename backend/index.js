e68e80fad0c55158d86d0811cef2545064b232d9
Hammad <hmdshfq@outlook.com>
2023-09-02 00:05:04 +0200

    feat: add comments

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,13 @@ import mongoose from 'mongoose';
 
 const app = express();
 
+// display the 'Welcome ...' message when the user visits the
+// localhost:5555/ page
 app.get('/', (req, res) => {
     return res.status(200).send('Welcome to MERN stack tutorial');
 })
 
+// Connect to mongodb database
 mongoose.connect(MONGODB_URL)
     .then(() => {
         console.log('App connected to database');
--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -1,5 +1,8 @@
 import mongoose from 'mongoose';
 
+// Create a book schema that tells the mongodb what kind of 
+// table to create in the database. In this schema we have 
+// four columns title, author, publish year, and timestamps
 const bookSchema = mongoose.Schema(
     {
         title: {
