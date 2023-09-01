bf6470d687d6b217b50bcc9a7689ef92a16a5554
Hammad <hmdshfq@outlook.com>
2023-09-01 23:36:20 +0200

    feat: listen to port and / path

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 import express from 'express';
+import { PORT } from './config.js';
 
 const app = express();
+
+app.get('/', (req, res) => {
+    return res.status(200).send('Welcome to MERN stack tutorial');
+})
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`);
+})
