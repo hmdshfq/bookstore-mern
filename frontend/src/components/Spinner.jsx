8ed9f9cce45da0bc357bec10370edde447ce96b6
Hammad <hmdshfq@outlook.com>
2023-09-03 12:37:05 +0200

    feat: add create book UI

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Spinner = () => {
     return (
-        <div className='fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center    '>
+        <div className='fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center'>
             <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-400'></div>
         </div>
     );
